eb86500a7c5a3cc956c7ca72b729967ebe001f21
Manu9084 <manusaini22092003@gmail.com>
2024-10-19 12:10:18 +0530

    manu navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close, github } from "../assets";
-import { a } from "maath/dist/objectSpread2-284232a6.esm";
-import linkedin from "../assets/linkedin.svg"
-import x from "../assets/x.svg"
+import linkedin from "../assets/linkedin.svg";
+import x from "../assets/x.svg";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -29,73 +28,76 @@ const Navbar = () => {
   }, []);
 
   return (
-    
     <nav
       className={`${
         styles.paddingX
       } w-full flex items-center py-5 fixed top-0 z-20 ${
         scrolled ? "bg-primary" : "bg-transparent"
       }`}
-    > 
+    >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <div className='flex'>
-        <Link
-          to='/'
-          className='flex items-center gap-2'
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-          }}
-        > 
-          <img src="mlogo.avif" alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Manu &nbsp;
-            <span className='sm:block hidden'> | Full-Stack Developer</span>
-          </p>
-        </Link>
-            
-            <div className="flex gap-4 ml-5">
-            <a href="https://www.linkedin.com/in/manu-saini-840515215/" target="_blank">
-                <img src={linkedin} alt="" className='w-9 h-9' />
-              </a>
-            <a href="https://github.com/ManuSaini9084"  target="_blank">
-                <img src={github} alt="" className='w-9 h-9' />
-              </a>
-            <a href="https://x.com/ManuSaini82045"  target="_blank">
-                <img src={x} alt="" className='w-9 h-9' />
-              </a>
-              </div>
-       
+          <Link
+            to='/'
+            className='flex items-center gap-2'
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src="mlogo.avif" alt='logo' className='w-9 h-9 object-contain' />
+            <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+              Manu &nbsp;
+              <span className='sm:block hidden'> | Full-Stack Developer</span>
+            </p>
+          </Link>
+
+          <div className="flex gap-4 ml-5">
+            <a href="https://www.linkedin.com/in/manu-saini-840515215/" target="_blank" aria-label="LinkedIn">
+              <img src={linkedin} alt="LinkedIn" className='w-9 h-9' />
+            </a>
+            <a href="https://github.com/ManuSaini9084" target="_blank" aria-label="GitHub">
+              <img src={github} alt="GitHub" className='w-9 h-9' />
+            </a>
+            <a href="https://x.com/ManuSaini82045" target="_blank" aria-label="Twitter/X">
+              <img src={x} alt="Twitter/X" className='w-9 h-9' />
+            </a>
+          </div>
         </div>
 
+        {/* Large Screen Menu */}
         <ul className='list-none hidden sm:flex flex-row gap-10'>
-  {navLinks.map((nav) => (
-    <li
-      key={nav.id}
-      className={`${
-        active === nav.title ? "text-white" : "text-secondary"
-      } hover:text-white text-[18px] font-medium cursor-pointer`}
-      onClick={() => setActive(nav.title)}
-    >
-      <a href={`#${nav.id}`}>{nav.title}</a>
-    </li>
-  ))}
-  <li
-    className={`${
-      active === 'Resume' ? "text-white" : "text-secondary"
-    } hover:text-white text-[18px] font-medium cursor-pointer`}
-  >
-    <a
-      href="https://drive.google.com/file/d/1870reVv-3iPOUDKmOOewwpgD2GmtrdSH/view?usp=sharing"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Resume
-    </a>
-  </li>
-</ul>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer relative group`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+              {/* Underline effect */}
+              <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-white group-hover:w-full transition-all duration-300"></span>
+            </li>
+          ))}
+          <li
+            className={`${
+              active === 'Resume' ? "text-white" : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer relative group`}
+          >
+            <a
+              href="https://drive.google.com/file/d/1870reVv-3iPOUDKmOOewwpgD2GmtrdSH/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
+            {/* Underline effect */}
+            <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-white group-hover:w-full transition-all duration-300"></span>
+          </li>
+        </ul>
 
-
+        {/* Mobile Menu */}
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img
             src={toggle ? close : menu}
@@ -124,6 +126,19 @@ const Navbar = () => {
                   <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
               ))}
+              <li
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === 'Resume' ? "text-white" : "text-secondary"
+                }`}
+              >
+                <a
+                  href="https://drive.google.com/file/d/1870reVv-3iPOUDKmOOewwpgD2GmtrdSH/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
           </div>
         </div>
